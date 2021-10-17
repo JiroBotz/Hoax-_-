d7e19d618e378c12b2a7acb6c5f26c76c2c6edab
AmelBotzz <87275421+AmelBotzz@users.noreply.github.com>
2021-10-17 18:33:02 +0700

    update api.js

--- routes/api.js
+++ routes/api.js
@@ -436,5 +436,280 @@ res.sendFile(invalidKey)
 }
 })
 
+router.get('/downloader/igdl', async (req, res, next) => {
+        var apikeyInput = req.query.apikey,
+                  url = req.query.url
+            
+	if(!apikeyInput) return res.json(loghandler.notparam)
+    if(!url) return res.json(loghandler.noturl)
+
+       if(listkey.includes(apikeyInput)){      
+       	
+       	function igDownload(url) {
+  return new Promise((resolve, reject) => {
+    const baseUrl = 'https://keeppost.com'
+    fetch(baseUrl, {
+      method: 'GET',
+      headers: {
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'sec-ch-ua': '"Google Chrome";v="93", " Not;A Brand";v="99", "Chromium";v="93"',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36'
+      }
+    })
+    .then(rsp => rsp.text())
+    .then((data) => {
+      const $ = cheerio.load(data)
+      const build_id = $('input[name="build_id"]').attr('value')
+      const build_key = $('input[name="build_key"]').attr('value')
+      const bodyForm = new formData()
+      bodyForm.append('url', url)
+      bodyForm.append('build_id', build_id)
+      bodyForm.append('build_key', build_key)
+      fetch(baseUrl + '/process.php', {
+        method: 'POST',
+        body: bodyForm,
+        headers: {
+          'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+          'sec-ch-ua': '"Google Chrome";v="93", " Not;A Brand";v="99", "Chromium";v="93"',
+          'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36'
+        }
+      })
+      .then(rsp => rsp.text())
+      .then((data) => {
+        const $$ = cheerio.load(data)
+        resolve({
+          url: $$('.success > a').attr('href')
+        })
+      })
+      .catch(reject)
+    })
+    .catch(reject)
+  })
+}
+                
+         igDownload(url)
+      .then(data => {
+      	var result = data;
+                 res.json({
+                 creator: 'Hafidz Abdillah',
+                 status: true,
+                 code: 200,
+                 message: 'Jangan ditembak bang',
+                 result
+             })
+          })
+          .catch(e => {
+         	res.json(loghandler.error)
+})
+    } else {
+res.sendFile(invalidKey)
+}
+})
+
+router.get('/downloader/igstory', async (req, res, next) => {
+        var apikeyInput = req.query.apikey,
+            username = req.query.username
+            
+	if(!apikeyInput) return res.json(loghandler.notparam)
+    if (!username) return res.json({ status : false, creator : `${creator}`, message : "Masukan parameter Username"})
+
+       if(listkey.includes(apikeyInput)){
+       
+
+            function igStory(username) {
+  return new Promise((resolve, reject) => {
+    const baseUrl = 'https://igmp4.com'
+    fetch(baseUrl + '/download-stories.php', {
+      method: 'GET',
+      headers: {
+        'cookie': 'PHPSESSID=ccb3ar0ul5jiu3knt2rh7pv4g4',
+        'sec-ch-ua': '"Google Chrome";v="93", " Not;A Brand";v="99", "Chromium";v="93"',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36'
+      }
+    })
+    .then(rsp => rsp.text())
+    .then((data) => {
+      const $ = cheerio.load(data)
+      const token = $('input[name="token"]').attr('value')
+      const bodyForm = new formData()
+      bodyForm.append('url', 'https://www.instagram.com/' + username)
+      bodyForm.append('action', 'story')
+      bodyForm.append('token', token)
+      bodyForm.append('json', '')
+      fetch(baseUrl + '/system/action.php', {
+        method: 'POST',
+        body: bodyForm,
+        headers: {
+          'cookie': 'PHPSESSID=ccb3ar0ul5jiu3knt2rh7pv4g4',
+          'sec-ch-ua': '"Google Chrome";v="93", " Not;A Brand";v="99", "Chromium";v="93"',
+          'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36'
+        }
+      })
+      .then(v => v.json())
+      .then((data) => {
+        resolve({
+          url_data: data.medias[0]
+        })
+      })
+      .catch(reject)
+    })
+    .catch(reject)
+  })
+}
+
+          igStory(username)
+        .then((data) => {
+        	 var result = data;
+             res.json({
+             	creator: 'Hafidz Abdillah',
+                 status: true,
+                 code: 200,
+                 message: 'Jangan ditembak bang',
+                result
+             })
+         })
+         .catch(e => {
+         	res.sendFile(error)
+})
+} else {
+res.sendFile(invalidKey)
+}
+})
+
+router.get('/downloader/twdl', async (req, res, next) => {
+        var apikeyInput = req.query.apikey,
+            url = req.query.url
+            
+	if(!apikeyInput) return res.json(loghandler.notparam)
+    if (!url) return res.json({ status : false, creator : `${creator}`, message : "Masukan parameter Url"})
+
+       if(listkey.includes(apikeyInput)){
+       
+
+            function twitterDown(url) {
+  return new Promise((resolve, reject) => {
+    const bodyForm = new formData()
+    bodyForm.append('url', url)
+    fetch('https://www.savetweetvid.com/downloader', {
+      method: 'POST',
+      body: bodyForm,
+      headers: {
+        'cookie': '_ga=GA1.2.1410189489.1630650655; _gid=GA1.2.95965189.1630650656; __gads=ID=8c5bb9c8c08571ab-22722b8c84cb001c:T=1630650656:RT=1630650656:S=ALNI_MYuAHfO543UXBFcCKILbqjWCEhpSg; XSRF-TOKEN=eyJpdiI6Im1UXC8zemE0UXFlYmJEbXoyNm1BeG5BPT0iLCJ2YWx1ZSI6InFNc0xOV0kxUHRaUG53VHdybHhOYnhPaTVMRWo5UExEVTJNSG4xOUFCQ2JEdjBvcGJGMUswS0pUZzNYTDAzNGFPUCtNMDFNOVp5REw2K3dTdUt5NG13PT0iLCJtYWMiOiIzODk5ZGVkM2I2ZjdjMTJhY2Y4NWI2MDY1OTc4OTVkZjFkMjhkOThhYmIyYzU5MzMxZjA0ZDEwNmQyZDViZDRlIn0%3D; laravel_session=eyJpdiI6IkRYK2M3U0VLSGxSQkFGTzRkdW9IeHc9PSIsInZhbHVlIjoiSEVMaUEyWTZUclU2MVN3NzFWdTQ0K2pQbFwvVVQ1d3gzTXJzakRYVFJtQlJvamwxczJnc2VqMHhid1QwdityMGJpd3J0M0h4bFI4RFA2Z09vUkg3T1BBPT0iLCJtYWMiOiJjN2U0ZDYzMTc3Zjg2OTEyOTVlNzgwNWRiMjZjNjMzMWM1NWMxODk3MTY2NjdhNTQ2Zjc0NzU5NTQzNGFjYzBiIn0%3D; __atuvc=5%7C35; __atuvs=6131c11f7c1d0b45004',
+        'sec-ch-ua': '"Google Chrome";v="93", " Not;A Brand";v="99", "Chromium";v="93"',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36'
+      }
+    })
+    .then(rsp => rsp.text())
+    .then((data) => {
+      const $ = cheerio.load(data)
+      resolve({
+          title: $('.card > .row > .col-md-4.col-sm-4').text().trim().replace(/\n/g, ' '),
+          url: $('.col-md-8.col-sm-8.col-table').find('a').eq(0).attr('href')
+      })
+    })
+    .catch(reject)
+  })
+}
+
+          twitterDown(url)
+        .then((data) => {
+        	 var result = data;
+             res.json({
+             	creator: 'Hafidz Abdillah',
+                 status: true,
+                 code: 200,
+                 message: 'Jangan ditembak bang',
+                result
+             })
+         })
+         .catch(e => {
+         	res.sendFile(error)
+})
+} else {
+res.sendFile(invalidKey)
+}
+})
+
+router.get('/downloader/tiktokdl', async (req, res, next) => {
+        var apikeyInput = req.query.apikey,
+            url = req.query.url
+            
+	if(!apikeyInput) return res.json(loghandler.notparam)
+    if (!url) return res.json({ status : false, creator : `${creator}`, message : "Masukan parameter url"})
+
+        if(listkey.includes(apikeyInput)){
+       	
+async function TiktokDownloader (Url) {
+    return new Promise (async (resolve, reject) => {
+    let get_data = await axios.get(Url, {
+        headers: {
+            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36',
+            'Cookie': 'tt_webid_v2=6986233640163411458; tt_webid=6986233640163411458; MONITOR_WEB_ID=6986233640163411458; passport_csrf_token_default=6c4582eb39e3a918b79bf19883e0383a; passport_csrf_token=6c4582eb39e3a918b79bf19883e0383a; passport_auth_status=b129aac2144e7d181e9206c77b829f33%2C; passport_auth_status_ss=b129aac2144e7d181e9206c77b829f33%2C; sid_guard=59bce8b9c36fd2ae7c65dc9ed1e57f4b%7C1626609635%7C5184000%7CThu%2C+16-Sep-2021+12%3A00%3A35+GMT; uid_tt=10f21da80e938dc4c74ac772a612ac900db73a510dce996f1e87038ed113d3b0; uid_tt_ss=10f21da80e938dc4c74ac772a612ac900db73a510dce996f1e87038ed113d3b0; sid_tt=59bce8b9c36fd2ae7c65dc9ed1e57f4b; sessionid=59bce8b9c36fd2ae7c65dc9ed1e57f4b; sessionid_ss=59bce8b9c36fd2ae7c65dc9ed1e57f4b; store-idc=alisg; store-country-code=id; tt_csrf_token=K1up8j9z7OmTrSfmvmoO1JP2; R6kq3TV7=AF7-iQB7AQAAqk-Wy_M1HqFLRc1VQ_I-5EkI1mYmYzKL9Nb5BnYDd5zx0dym|1|0|c5260d1687443f9982544406597db77fb23ffc52; s_v_web_id=verify_krsv2o62_iDlMefgy_hytF_4I87_9jYh_Ujpnwxam1TNO; cmpl_token=AgQQAPOgF-RMpbDNIuufsZ04-pWPSIhMP4ArYP5gWA; odin_tt=04cf5d9f07b62e050c679ab203370816484a861d3efc80bd5d0db6fd4b0c4a6018acdd2d4b2b814e487f25ba678a53103e29abb336446c456ee9dfff04a0a789bdc6b29b4685b81aa50161179b68c98b; ttwid=1%7C5hEmJiL5W2c3q3WVqHbF85173NjIdJm9ZJJPEvxQeyI%7C1627801693%7C48ff7670e842ddd8b838a35541a94891eb144539cba2b0cb308b20254e6fe089; passport_fe_beating_status=false'
+        }
+    })
+
+    //Getting the element
+    let $ = require('cheerio').load(get_data.data)
+    let jsnya = JSON.parse($('script').eq(6).get()[0].children[0].data)
+    let t = jsnya.props.pageProps.itemInfo.itemStruct
+
+	await axios.request({
+	    url: "https://ttdownloader.com/",
+	    method: "GET",
+	    headers: {
+		"accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+		"accept-language": "en-US,en;q=0.9,id;q=0.8",
+		"user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36",
+		"cookie": "_ga=GA1.2.1240046717.1620835673; PHPSESSID=i14curq5t8omcljj1hlle52762; popCookie=1; _gid=GA1.2.1936694796.1623913934"
+	    }
+	}).then( respon => {
+	    const $ = cheerio.load(respon.data)
+	    const token = $('#token').attr('value')
+	    axios({
+		url: "https://ttdownloader.com/req/",
+		method: "POST",
+		data: new URLSearchParams(Object.entries({url: Url, format: "", token: token})),
+		headers: {
+		    "accept": "*/*",
+		    "accept-language": "en-US,en;q=0.9,id;q=0.8",
+		    "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+		    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36",
+		    "cookie": "_ga=GA1.2.1240046717.1620835673; PHPSESSID=i14curq5t8omcljj1hlle52762; popCookie=1; _gid=GA1.2.1936694796.1623913934"
+		}
+	    }).then( res => {
+		const ch = cheerio.load(res.data)
+		const result = {
+		    username: t.author.uniqueId,
+		    nickname: t.author.nickname,
+        description: t.desc,
+        views: t.stats.playCount,
+        likes: t.stats.diggCount,
+        comments: t.stats.commentCount,
+        shares: t.stats.shareCount,
+           thumbnail: t.video.cover,
+			     nowatermark: ch('#results-list > div:nth-child(2)').find('div.download > a').attr('href'),
+			     watermark: ch('#results-list > div:nth-child(3)').find('div.download > a').attr('href'),
+			     audio: t.music.playUrl
+		}
+		resolve(result)
+	    }).catch(reject)
+	}).catch(reject)
+    })
+}
+            TiktokDownloader(url)
+            .then((result) => {
+                res.json({
+             	creator: 'Hafidz Abdillah',
+                 status: true,
+                 code: 200,
+                 message: 'Jangan ditembak bang',
+                 result
+            })
+         })
+} else {
+res.sendFile(invalidKey)
+}
+})
+
 // End of script
 module.exports = router
