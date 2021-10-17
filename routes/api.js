eb6b8e546766457fbef7d63712a684e19bf0ed77
AmelBotzz <87275421+AmelBotzz@users.noreply.github.com>
2021-10-17 18:00:09 +0700

    update api.js

--- routes/api.js
+++ routes/api.js
@@ -382,45 +382,87 @@ router.get('/downloader/ytmp4', async (req, res, next) => {
 
       if(listkey.includes(apikeyInput)){
              
-         async function ytDonlodMp4(url) {
-  return new Promise((resolve, reject) => {
-    try {
-      const id = yt.getVideoID(url)
-      const yutub = yt.getInfo(`https://www.youtube.com/watch?v=${id}`)
-      .then((data) => {
-        let pormat = data.formats
-        let video = []
-        for (let i = 0; i < pormat.length; i++) {
-          if (pormat[i].container == 'mp4' && pormat[i].hasVideo == true && pormat[i].hasAudio == true) {
-            let vid = pormat[i]
-            video.push(vid.url)
-          }
-        }
-        const title = data.player_response.microformat.playerMicroformatRenderer.title.simpleText
-        const thumb = data.player_response.microformat.playerMicroformatRenderer.thumbnail.thumbnails[0].url
-        const channel = data.player_response.microformat.playerMicroformatRenderer.ownerChannelName
-        const views = data.player_response.microformat.playerMicroformatRenderer.viewCount
-        const published = data.player_response.microformat.playerMicroformatRenderer.publishDate
-        
-        const result = {
-          title: title,
-          thumb: thumb,
-          channel: channel,
-          published: published,
-          views: views,
-          url: video[0]
-        }
-        return(result)
-      })
-      resolve(yutub)
-    } catch (error) {
-        reject(error);
-      }
-      console.log(error)
-  })
+         function youtube(link){
+	return new Promise((resolve, reject) => {
+		const ytIdRegex = /(?:http(?:s|):\/\/|)(?:(?:www\.|)youtube(?:\-nocookie|)\.com\/(?:watch\?.*(?:|\&)v=|embed\/|v\/)|youtu\.be\/)([-_0-9A-Za-z]{11})/
+		if (ytIdRegex.test(link)) {
+		let url =  ytIdRegex.exec(link)
+		let config = {
+			'url': 'https://www.youtube.be/' + url,
+			'q_auto': 0,
+			'ajax': 1
+		}
+		let headerss = 	{
+			"sec-ch-ua": '" Not;A Brand";v="99", "Google Chrome";v="91", "Chromium";v="91"',
+			"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+			"Cookie": 'PHPSESSID=6jo2ggb63g5mjvgj45f612ogt7; _ga=GA1.2.405896420.1625200423; _gid=GA1.2.2135261581.1625200423; _PN_SBSCRBR_FALLBACK_DENIED=1625200785624; MarketGidStorage={"0":{},"C702514":{"page":5,"time":1625200846733}}'
+		}
+	axios('https://www.y2mate.com/mates/en68/analyze/ajax',{
+			method: 'POST',
+			data: new URLSearchParams(Object.entries(config)),
+			headers: headerss
+		})
+	.then(({ data }) => {
+		const $ = cheerio.load(data.result)
+		let img = $('div.thumbnail.cover > a > img').attr('src');
+		let title = $('div.thumbnail.cover > div > b').text();
+		let size = $('#mp4 > table > tbody > tr:nth-child(3) > td:nth-child(2)').text()
+		let size_mp3 = $('#audio > table > tbody > tr:nth-child(1) > td:nth-child(2)').text()
+		let id = /var k__id = "(.*?)"/.exec(data.result)[1]
+		let configs = {
+    type: 'youtube',
+    _id: id,
+    v_id: url[1],
+    ajax: '1',
+    token: '',
+    ftype: 'mp4',
+    fquality: 480
+  }
+	axios('https://www.y2mate.com/mates/en68/convert',{
+		method: 'POST',
+		data: new URLSearchParams(Object.entries(configs)),
+		headers: headerss 
+	})
+	.then(({data}) => {
+		const $ = cheerio.load(data.result)
+		let link = $('div > a').attr('href')
+	let configss = {
+    type: 'youtube',
+    _id: id,
+    v_id: url[1],
+    ajax: '1',
+    token: '',
+    ftype: 'mp3',
+    fquality: 128
+  }
+	axios('https://www.y2mate.com/mates/en68/convert',{
+		method: 'POST',
+		data: new URLSearchParams(Object.entries(configss)),
+		headers: headerss 
+	})
+	.then(({ data }) => {
+		const $ = cheerio.load(data.result)
+		let audio = $('div > a').attr('href')
+		resolve({
+			id: url[1],
+			title: title,
+			size: size,
+			quality: '480p',
+			thumb: img,
+			link: link,
+			size_mp3: size_mp3,
+			mp3: audio
+		})
+
+		})
+			})
+		})
+	.catch(reject)
+	}else reject('link invalid')
+	})
 }
 
-          ytDonlodMp4(url)
+          youtube(url)
         .then((data) => {
         var result = data;
              res.json({
