bb8aaa8c3e1fcb8993e42ea12b73f07f8c47c207
AmelBotzz <87275421+AmelBotzz@users.noreply.github.com>
2021-10-21 00:06:05 +0700

    update api.js

--- routes/api.js
+++ routes/api.js
@@ -1328,41 +1328,12 @@ router.get('/search/growstock', async (req, res, next) => {
         if(!query) return res.json({ status : false, creator : `${creator}`, message : "Masukan parameter query"})
 
        if(listkey.includes(apikeyInput)){      
-       	
-       	const growstockSearch = (query) => new Promise((resolve, reject) => {
-                    fetch(`https://growstocks.xyz/search?item=${query}`, {
-                        "credentials": "include",
-                        "headers": {
-                            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0",
-                            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-                            "Accept-Language": "en-US,en;q=0.5",
-                            "Upgrade-Insecure-Requests": "1",
-                            "Pragma": "no-cache",
-                            "Cache-Control": "no-cache"
-                        },
-                        "referrer": "https://growstocks.xyz/",
-                        "method": "GET",
-                        "mode": "cors"
-                    }).then((res) => res.data())
-                        .then((data) => {
-                            const $ = cheerio.load(data)
-                            const dataArr = [];
-                            $('div.searchRes').each((i, el) => {
-                                const title = $(el).find('div > div > h2 > a').text().trim();
-                                const price = $(el).find('div > div > p:nth-child(3) > b:nth-child(1)').text().trim();
-                                const editedAt = $(el).find('div > div > p:nth-child(5) > b:nth-child(1)').text().trim();
-                                dataArr.push({
-                                    title: title,
-                                    price: price ? price : 'Data tidak terbaca',
-                                    editedAt: editedAt ? editedAt : 'Data tidak terbaca',
-                                })
-                            })
-                            resolve(dataArr)
-                        })
-                })
-
-      growstockSearch(query)
-      .then((data) => {
+       	try {
+       	gplay.search({
+                        term: q,
+                        num: 1
+                    })
+          .then(async (data) => {
       	var result = data;
      res.json({
                  creator: 'Hafidz Abdillah',
@@ -1374,6 +1345,7 @@ router.get('/search/growstock', async (req, res, next) => {
           })
     } else {
 res.sendFile(invalidKey)
+}
 }
 })
 // End of script
