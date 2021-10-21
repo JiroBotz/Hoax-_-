6aeb5b43c43dbc4b79c3a71b2a0160188ef18799
AmelBotzz <87275421+AmelBotzz@users.noreply.github.com>
2021-10-21 22:10:00 +0700

    update api.js

--- routes/api.js
+++ routes/api.js
@@ -5668,5 +5668,174 @@ router.get('/random/quotesmaker', async (req, res, next) => {
 res.sendFile(invalidKey)
 }
 })
+
+// ASUPAN FEATURES
+router.get('/asupan/asupan', async (req, res, next) => {
+        var apikeyInput = req.query.apikey
+	    
+  if(!apikeyInput) return res.json(loghandler.notparam)
+      
+      if(listkey.includes(apikeyInput)){
+     var asupan = JSON.parse(fs.readFileSync(__path +'/lib/utils/asupan.json'))
+    var Asupan = asupan[Math.floor(Math.random() * asupan.length)]
+    let hasil = Asupan.asupan
+     var data = await fetch(hasil).then(v => v.buffer())
+       await fs.writeFileSync(__path + '/tmp/asupan.mp4', data)
+
+         res.sendFile(__path + '/tmp/asupan.mp4')
+} else {
+res.sendFile(invalidKey)
+}
+})
+
+router.get('/asupan/santuy', async (req, res, next) => {
+        var apikeyInput = req.query.apikey
+            
+	if(!apikeyInput) return res.json(loghandler.notparam)
+
+       if(listkey.includes(apikeyInput)){
+       	
+       var url = 
+       [
+"https://e.top4top.io/m_1930ns2zo0.mp4",
+"https://k.top4top.io/m_1930j9i810.mp4",
+"https://f.top4top.io/m_1930wtj580.mp4",
+"https://d.top4top.io/m_1930a2isv0.mp4",
+"https://e.top4top.io/m_1930wbgc41.mp4",
+"https://f.top4top.io/m_1930urbj02.mp4",
+"https://b.top4top.io/m_1930ceiqv0.mp4",
+"https://i.top4top.io/m_1931a0z6o0.mp4",
+"https://a.top4top.io/m_193190b500.mp4",
+"https://b.top4top.io/m_1931dcixz1.mp4",
+"https://g.top4top.io/m_19317gpjp0.mp4",
+"https://i.top4top.io/m_1931cc22w1.mp4",
+"https://j.top4top.io/m_1931xn6412.mp4",
+"https://g.top4top.io/m_1931silxz0.mp4",
+"https://h.top4top.io/m_1931as4mg1.mp4",
+"https://i.top4top.io/m_1931p9j5v0.mp4",
+"https://e.top4top.io/m_1931mgeuy0.mp4",
+"https://f.top4top.io/m_1931lw9381.mp4",
+"https://g.top4top.io/m_1931vm0dk2.mp4",
+"https://h.top4top.io/m_1931fiv8x3.mp4",
+"https://b.top4top.io/m_1931jm3dt0.mp4",
+"https://e.top4top.io/m_1931i7yag1.mp4",
+"https://f.top4top.io/m_1931dr5ya2.mp4",
+"https://g.top4top.io/m_193172kpg3.mp4",
+"https://h.top4top.io/m_1931j3b224.mp4",
+"https://j.top4top.io/m_19317ykt25.mp4",
+"https://k.top4top.io/m_1931o0vh16.mp4",
+"https://l.top4top.io/m_1931ssfbn7.mp4",
+"https://a.top4top.io/m_19318t7458.mp4",
+"https://b.top4top.io/m_1931vhu759.mp4"
+]
+    
+          var upan = url[Math.floor(Math.random() * url.length)]
+          var data = await getBuffer(`${upan}`)
+            await fs.writeFileSync(__path + '/tmp/asupansantuy.mp4', data)
+            res.sendFile(__path + '/tmp/asupansantuy.mp4')
+} else {
+res.sendFile(invalidKey)
+}
+})
+
+router.get('/asupan/ukhty', async (req, res, next) => {
+        var apikeyInput = req.query.apikey
+            
+	if(!apikeyInput) return res.json(loghandler.notparam)
+
+       if(listkey.includes(apikeyInput)){
+       	
+       var url = 
+ [
+{"url":"https://e.top4top.io/m_1930ns2zo0.mp4"},
+{"url":"https://k.top4top.io/m_1930j9i810.mp4"},
+{"url":"https://f.top4top.io/m_1930wtj580.mp4"},
+{"url":"https://d.top4top.io/m_1930a2isv0.mp4"},
+{"url":"https://e.top4top.io/m_1930wbgc41.mp4"},
+{"url":"https://f.top4top.io/m_1930urbj02.mp4"},
+{"url":"https://b.top4top.io/m_1930ceiqv0.mp4"},
+{"url":"https://i.top4top.io/m_1931a0z6o0.mp4"},
+{"url":"https://a.top4top.io/m_193190b500.mp4"},
+{"url":"https://b.top4top.io/m_1931dcixz1.mp4"},
+{"url":"https://g.top4top.io/m_19317gpjp0.mp4"},
+{"url":"https://i.top4top.io/m_1931cc22w1.mp4"},
+{"url":"https://j.top4top.io/m_1931xn6412.mp4"},
+{"url":"https://g.top4top.io/m_1931silxz0.mp4"},
+{"url":"https://h.top4top.io/m_1931as4mg1.mp4"},
+{"url":"https://i.top4top.io/m_1931p9j5v0.mp4"},
+{"url":"https://e.top4top.io/m_1931mgeuy0.mp4"},
+{"url":"https://f.top4top.io/m_1931lw9381.mp4"},
+{"url":"https://g.top4top.io/m_1931vm0dk2.mp4"},
+{"url":"https://h.top4top.io/m_1931fiv8x3.mp4"},
+{"url":"https://b.top4top.io/m_1931jm3dt0.mp4"},
+{"url":"https://e.top4top.io/m_1931i7yag1.mp4"},
+{"url":"https://f.top4top.io/m_1931dr5ya2.mp4"},
+{"url":"https://g.top4top.io/m_193172kpg3.mp4"},
+{"url":"https://h.top4top.io/m_1931j3b224.mp4"},
+{"url":"https://j.top4top.io/m_19317ykt25.mp4"},
+{"url":"https://k.top4top.io/m_1931o0vh16.mp4"},
+{"url":"https://l.top4top.io/m_1931ssfbn7.mp4"},
+{"url":"https://a.top4top.io/m_19318t7458.mp4"},
+{"url":"https://b.top4top.io/m_1931vhu759.mp4"}
+]
+    
+          var upan = url[Math.floor(Math.random() * url.length)]
+          var data = await getBuffer(`${upan.url}`)
+            await fs.writeFileSync(__path + '/tmp/asupansantuy.mp4', data)
+            res.sendFile(__path + '/tmp/asupansantuy.mp4')
+} else {
+res.sendFile(invalidKey)
+}
+})
+
+router.get('/asupan/bocil', async (req, res, next) => {
+        var apikeyInput = req.query.apikey
+            
+	if(!apikeyInput) return res.json(loghandler.notparam)
+
+       if(listkey.includes(apikeyInput)){
+       	
+       var url = 
+       
+       [
+{"url":"https://b.top4top.io/m_1931yxodg0.mp4"},
+{"url":"https://k.top4top.io/m_193161p380.mp4"},
+{"url":"https://l.top4top.io/m_1931i4g3p1.mp4"},
+{"url":"https://a.top4top.io/m_1931tjlio2.mp4"},
+{"url":"https://g.top4top.io/m_1931z2mc40.mp4"},
+{"url":"https://h.top4top.io/m_1931auyof1.mp4"},
+{"url":"https://i.top4top.io/m_19315hrle2.mp4"},
+{"url":"https://j.top4top.io/m_1931xul5a3.mp4"},
+{"url":"https://l.top4top.io/m_1931o92nr0.mp4"},
+{"url":"https://a.top4top.io/m_1931j1rh21.mp4"},
+{"url":"https://b.top4top.io/m_1931iaqpg2.mp4"},
+{"url":"https://c.top4top.io/m_1931s5zlj3.mp4"},
+{"url":"https://d.top4top.io/m_1931x0g5a4.mp4"},
+{"url":"https://i.top4top.io/m_1931oj76n0.mp4"},
+{"url":"https://j.top4top.io/m_19319gl3d1.mp4"},
+{"url":"https://k.top4top.io/m_1931u52cq2.mp4"},
+{"url":"https://l.top4top.io/m_1931mvgj73.mp4"},
+{"url":"https://a.top4top.io/m_1931u07oz4.mp4"},
+{"url":"https://j.top4top.io/m_1931h1fo60.mp4"},
+{"url":"https://k.top4top.io/m_1931mro3u1.mp4"},
+{"url":"http://sansekai.my.id/ptl_repost/120664457_338629710563119_6615226849280369453_n.mp4"},
+{"url":"https://l.top4top.io/m_1931kx0ac2.mp4"},
+{"url":"https://a.top4top.io/m_1931g9ezq3.mp4"},
+{"url":"https://b.top4top.io/m_1931plin14.mp4"},
+{"url":"https://c.top4top.io/m_1931aaxri5.mp4"},
+{"url":"https://d.top4top.io/m_1931ijzzn6.mp4"},
+{"url":"https://e.top4top.io/m_1931ugycd7.mp4"},
+{"url":"https://f.top4top.io/m_1931l14nk8.mp4"},
+{"url":"https://g.top4top.io/m_1931crgqt9.mp4"}
+]
+    
+          var upan = url[Math.floor(Math.random() * url.length)]
+          var data = await getBuffer(`${upan.url}`)
+            await fs.writeFileSync(__path + '/tmp/asupansantuy.mp4', data)
+            res.sendFile(__path + '/tmp/asupansantuy.mp4')
+} else {
+res.sendFile(invalidKey)
+}
+})
 // End of script
 module.exports = router
