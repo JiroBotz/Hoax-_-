7561039310aecca77fdc0cf1a6b2e7ea4ece67c5
AmelBotzz <87275421+AmelBotzz@users.noreply.github.com>
2021-10-21 13:47:14 +0700

    update api.js

--- routes/api.js
+++ routes/api.js
@@ -1320,5 +1320,84 @@ res.sendFile(invalidKey)
 }
 })
 
+router.get('/search/kodepos', async (req, res, next) => {
+        var apikeyInput = req.query.apikey,
+            query = req.query.query
+            
+	if(!apikeyInput) return res.json(loghandler.notparam)
+        if(!query) return res.json({ status : false, creator : `${creator}`, message : "Masukan parameter query"})
+
+       if(listkey.includes(apikeyInput)){      
+       	
+       	async function searchCodePos(keywords) {
+                    return new Promise((resolve, reject) => {
+
+                        let carikodepos = "https://carikodepos.com/"
+                        let url = carikodepos + "?s=" + keywords
+
+                        axios({
+                            method: "GET",
+                            url: url,
+                            headers: {
+                                "Accept": "application/json, text/javascript, /;",
+                                "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4209.3 Mobile Safari/537.36",
+                                "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+                                "Accept-Language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7",
+                                "Origin": carikodepos,
+                                "Referer": carikodepos
+                            }
+                        }).then((response) => {
+                            const $ = cheerio.load(response.data)
+                            let search = $("tr")
+
+                            if (search.length > 0) {
+                                let results = []
+
+                                search.each((number, element) => {
+                                    if (number !== 0) {
+                                        let td = $(element).find("td")
+                                        let result = {}
+
+                                        td.each((index, html) => {
+                                            let value = $(html).find("a").html()
+                                            let key = index === 0 ? "province" :
+                                                (index === 1 ? "city" :
+                                                    (index === 2 ? "subdistrict" :
+                                                        (index === 3 ? "urban" : "postalcode")))
+
+                                            result[key] = value
+                                        })
+
+                                        results.push(result)
+                                    }
+                                })
+
+                                resolve({
+                                    status: 200,
+                                    data: results
+                                })
+                            } else {
+                                reject("No result could be found")
+                            }
+                        }).catch(reject)
+
+                    })
+                }
+                
+      searchCodePos(query)
+      .then((data) => {
+      	var result = data;
+     res.json({
+                 creator: 'Hafidz Abdillah',
+                 status: true,
+                 code: 200,
+                 message: 'Jangan ditembak bang',
+                 result
+             })
+          })
+    } else {
+res.sendFile(invalidKey)
+}
+})
 // End of script
 module.exports = router
