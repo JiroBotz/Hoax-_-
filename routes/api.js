09b3fc66b6ec03ed22b0f14b63f196400e12e8ca
AmelBotzz <87275421+AmelBotzz@users.noreply.github.com>
2021-10-28 22:29:44 +0700

    update api.js

--- routes/api.js
+++ routes/api.js
@@ -329,8 +329,8 @@ router.get('/downloader/fbdl', async (req, res, next) => {
 "cookie": "_ga=GA1.2.1310699039.1624884412; _pbjs_userid_consent_data=3524755945110770; cto_bidid=rQH5Tl9NNm5IWFZsem00SVVuZGpEd21sWnp0WmhUeTZpRXdkWlRUOSUyQkYlMkJQQnJRSHVPZ3Fhb1R2UUFiTWJuVGlhVkN1TGM2anhDT1M1Qk0ydHlBb21LJTJGNkdCOWtZalRtZFlxJTJGa3FVTG1TaHlzdDRvJTNE; cto_bundle=g1Ka319NaThuSmh6UklyWm5vV2pkb3NYaUZMeWlHVUtDbVBmeldhNm5qVGVwWnJzSUElMkJXVDdORmU5VElvV2pXUTJhQ3owVWI5enE1WjJ4ZHR5NDZqd1hCZnVHVGZmOEd0eURzcSUyQkNDcHZsR0xJcTZaRFZEMDkzUk1xSmhYMlY0TTdUY0hpZm9NTk5GYXVxWjBJZTR0dE9rQmZ3JTNEJTNE; _gid=GA1.2.908874955.1625126838; __gads=ID=5be9d413ff899546-22e04a9e18ca0046:T=1625126836:RT=1625126836:S=ALNI_Ma0axY94aSdwMIg95hxZVZ-JGNT2w; cookieconsent_status=dismiss"
 	}
 })
-	.then(async({ data }) => {
-const $ = cheerio.load(data);	
+	.then(async({ result }) => {
+const $ = cheerio.load(result.data);	
 resolve({
 	video: $('div.col-md-4.btns-download > p:nth-child(1) > a').attr('href'),
 	audio: $('div.col-md-4.btns-download > p:nth-child(3) > a').attr('href')
